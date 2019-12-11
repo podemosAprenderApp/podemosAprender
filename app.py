4b1eaa3612c9cd7c8a5b872e0ca976a51c80ebbb
Leandro Barone <web@leandrobarone.com.ar>
2019-12-11 01:20:53 -0300

    Servidor con Gunicorn

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from datetime import datetime
-from http.server import HTTPServer, BaseHTTPRequestHandler
 
-HOST_NAME = 'localhost'
-PORT_NUMBER = 443
-
-class Server(BaseHTTPRequestHandler):
-	def do_GET(self):
-		content = self.handle_http(200, 'text/html')
-		self.wfile.write(content)
-    
-	def handle_http(self, status, content_type):
-		self.send_response(status)
-		self.send_header('Content-type', content_type)
-		self.end_headers()
-		return bytes(datetime.now().strftime('Hola, mundo! Es el %d/%m/%Y a las %H:%M:%S'), 'UTF-8')
-
-httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
-print('Servidor iniciado %s:%s' % (HOST_NAME, PORT_NUMBER))
-httpd.serve_forever()
+def app(environ, start_response):
+	data = bytes(datetime.now().strftime('Hola, mundo! Es el %d/%m/%Y a las %H:%M:%S'), 'UTF-8')
+	start_response("200 OK", [
+		("Content-Type", "text/plain"),
+		("Content-Length", str(len(data)))
+	])
+	return iter([data])
--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: gunicorn -w 3 app:app
