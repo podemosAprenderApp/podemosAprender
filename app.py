b4fdfd49559f510fcdd5319cee937b3942266811
Leandro Barone <web@leandrobarone.com.ar>
2019-12-11 01:14:02 -0300

    puerto 443

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+HOST_NAME = 'localhost'
+PORT_NUMBER = 443
+
 class Server(BaseHTTPRequestHandler):
 	def do_GET(self):
 		content = self.handle_http(200, 'text/html')
@@ -11,9 +14,6 @@ class Server(BaseHTTPRequestHandler):
 		self.send_header('Content-type', content_type)
 		self.end_headers()
 		return bytes(datetime.now().strftime('Hola, mundo! Es el %d/%m/%Y a las %H:%M:%S'), 'UTF-8')
-
-HOST_NAME = 'localhost'
-PORT_NUMBER = 80
 
 httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
 print('Servidor iniciado %s:%s' % (HOST_NAME, PORT_NUMBER))
