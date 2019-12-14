ea32e8fa052103ecc0dbaed89d060bfdca3ba012
Dario <dariokozicki@hotmail.com>
2019-12-14 00:16:16 -0300

    Agregado el servidor inicial de Django. Se configuro para heroku siguiendo esta guia https://medium.com/@BennettGarner/deploying-django-to-heroku-procfile-static-root-other-pitfalls-e7ab8b2ba33b

--- app.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def app(environ, start_response):
-	data = bytes(datetime.now().strftime('Hola, mundo! Es el %d/%m/%Y a las %H:%M:%S'), 'UTF-8')
-	start_response("200 OK", [
-		("Content-Type", "text/plain"),
-		("Content-Length", str(len(data)))
-	])
-	return iter([data])
