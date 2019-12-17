dc51df48f5a972caf17c386ef4bae7e0c1b03a31
Dario <dariokozicki@hotmail.com>
2019-12-16 22:57:30 -0300

    Removed environment

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dario\documents\python scripts\podemosaprender\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
