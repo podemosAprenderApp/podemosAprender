d14aa25351575d532525f840a373e59d398fcc0d
Leandro Barone <web@leandrobarone.com.ar>
2019-12-17 00:10:32 -0300

    Static files

--- PodemosAprenderApp/settings.py
+++ PodemosAprenderApp/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "staticfiles")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
