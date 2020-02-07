b41e1cf3df18a3b6aa35c9d3f985d17ea7863505
Dario <dariokozicki@hotmail.com>
2020-02-07 01:08:20 -0300

    Added heroku-friendly port!

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,8 +5,16 @@ import spark.debug.DebugScreen;
 
 public class Server {
     public static void main(String[] args){
-        Spark.port(9000);
+        Spark.port(getHerokuAssignedPort());
         Router.init();
         DebugScreen.enableDebugScreen();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 9000; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
